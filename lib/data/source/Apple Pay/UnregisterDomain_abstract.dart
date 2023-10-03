756f2a54e726a3b7b395d05d64dd02eb8568b80c
Cashkid500 <Murtadhob@gmail.com>
2023-10-03 23:26:40 +0100

    Created abstract file for the consumption of PayStack API

--- lib/data/source/Apple Pay/UnregisterDomain_abstract.dart
+++ lib/data/source/Apple Pay/UnregisterDomain_abstract.dart
@@ -1,7 +1,7 @@
 import 'package:kita_kerja/models/Apple%20Pay/body/unregisterDomain.dart';
 import 'package:kita_kerja/models/Apple%20Pay/response/unregisterDomainResponse.dart';
 
-abstract class ApplePaySource {
+abstract class UnregisterDomainSource {
   Future<UnregisterDomainResponse> unregisterDomain({
     required UnregisterDomain payload,
   });
