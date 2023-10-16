321763471146cf89a9c809a78698e78b66116370
Cashkid500 <Murtadhob@gmail.com>
2023-10-16 16:48:28 +0100

    Source File Updated

--- lib/data/source/Apple Pay/unregisterDomain_source.dart
+++ lib/data/source/Apple Pay/unregisterDomain_source.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+
+import 'package:kita_kerja/constants/app_api_endpoints.dart';
 import 'package:kita_kerja/core/network/network_request.dart';
 import 'package:kita_kerja/core/network/network_retry.dart';
 import 'package:kita_kerja/data/source/Apple%20Pay/unregisterDomain_abstract.dart';
@@ -13,11 +16,46 @@ class UnregisterDomainSourceImpl implements UnregisterDomainSource {
     required this.networkRetry,
   });
 
+  // @override
+  // Future<UnregisterDomainResponse> unregisterDomain({required UnregisterDomain payload}) {
+  //   // TODO: implement unregisterDomain
+  //   throw UnimplementedError();
+  // }
+
   @override
-  Future<UnregisterDomainResponse> unregisterDomain({required UnregisterDomain payload}) {
-    // TODO: implement unregisterDomain
-    throw UnimplementedError();
+  Future<UnregisterDomainResponse> unregisterDomain(
+      {required UnregisterDomain payload}) async {
+    String url = AppEndpoints.unregisterDomain;
+    final body = {
+      "domainName": payload.domainName,
+    };
+    final response = await networkRetry.networkRetry(
+      () => networkRequest.delete(
+        url,
+        body: body,
+      ),
+    );
+    final data = await json.decode(response.body);
+    if (response.statusCode == 200) {
+      try {
+        final UnregisterDomainResponse responseModel =
+            UnregisterDomainResponse.fromJson(data);
+        return responseModel;
+      } on Exception catch (_) {
+        rethrow;
+      }
+    } else {
+      final UnregisterDomainResponse responseModel = UnregisterDomainResponse.fromJson(data);
+      try {
+        if (responseModel.status == false) {
+          throw Exception(data['message']);
+        } else {
+          final errorMessage = data['message'];
+          throw Exception("API failed");
+        }
+      } on Exception catch (_) {
+        rethrow;
+      }
+    }
   }
-
-
 }
