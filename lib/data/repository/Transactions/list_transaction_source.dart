9c232ce1a8d6a6de62730fc5a8649a8ccf3e6a7e
Cashkid500 <Murtadhob@gmail.com>
2023-10-23 17:56:57 +0100

    Repository Implementations Files  Done

---
+++ lib/data/repository/Transactions/list_transaction_source.dart
@@ -0,0 +1,28 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:kita_kerja/core/failures/failures.dart';
+import 'package:kita_kerja/core/network/network_info.dart';
+import 'package:kita_kerja/core/runner/service.dart';
+import 'package:kita_kerja/data/repository/Transactions/list_transaction_abstract.dart';
+import 'package:kita_kerja/data/source/Transactions/listTransaction_abstract.dart';
+import 'package:kita_kerja/models/Transactions/response/listTransactionResponse.dart';
+import 'package:kita_kerja/providers/network_provider.dart';
+import 'package:kita_kerja/providers/source_provider.dart';
+
+class ListTransactionRepoImpl implements ListTransactionRepo{
+  final NetworkInfo list;
+  final ListTransactionSource transaction;
+
+  ListTransactionRepoImpl({required Ref ref})
+      : transaction = ref.read(listTransactionSourceProvider),
+        list = ref.read(networkInfoProvider);
+
+  @override
+  Future<Either<Failure, ListTransactionResponse>> listTransaction() async {
+    ServiceRunner<Failure, ListTransactionResponse> sR = ServiceRunner(list);
+
+    return sR.tryRemoteandCatch(
+        call: transaction.listTransaction(), 
+        errorTitle: "Transaction Not Listed");
+  }
+}
---
+++ lib/data/repository/Transactions/view_transaction_timeline_source.dart
@@ -0,0 +1,28 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:kita_kerja/core/failures/failures.dart';
+import 'package:kita_kerja/core/network/network_info.dart';
+import 'package:kita_kerja/core/runner/service.dart';
+import 'package:kita_kerja/data/repository/Transactions/view_transaction_timeline_abstract.dart';
+import 'package:kita_kerja/data/source/Transactions/viewTransactionTimeline_abstract.dart';
+import 'package:kita_kerja/models/Transactions/response/viewTransactionTimelineResponse.dart';
+import 'package:kita_kerja/providers/network_provider.dart';
+import 'package:kita_kerja/providers/source_provider.dart';
+
+class ViewTransactionTimelineRepoImpl implements ViewTransactionTimelineRepo{
+  final NetworkInfo view;
+  final ViewTransactionTimelineSource transaction;
+
+  ViewTransactionTimelineRepoImpl({required Ref ref})
+      : transaction = ref.read(viewTransactionTimelineSourceProvider),
+        view = ref.read(networkInfoProvider);
+
+  @override
+  Future<Either<Failure, ViewTransactionTimelineResponse>> viewTransactionTimeline() async {
+    ServiceRunner<Failure, ViewTransactionTimelineResponse> sR = ServiceRunner(view);
+
+    return sR.tryRemoteandCatch(
+        call: transaction.viewTransactionTimeline(), 
+        errorTitle: "Transaction Timeline Not Viewed");
+  }
+}
