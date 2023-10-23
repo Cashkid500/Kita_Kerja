f31d692e5297d2eb36bbff97c3281dcb0dfea3fa
Balogun Murtadho <murtadhob@gmail.com>
2023-10-23 07:18:28 +0100

    Updated

--- lib/providers/source_provider.dart
+++ lib/providers/source_provider.dart
@@ -1,7 +1,7 @@
-// import 'package:flutter_riverpod/flutter_riverpod.dart';
-// import 'package:kita_kerja/data/source/Apple%20Pay/UnregisterDomain_abstract.dart';
-// import 'package:kita_kerja/data/source/Apple%20Pay/UnregisterDomain_source.dart';
-// import 'package:kita_kerja/providers/network_provider.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:kita_kerja/data/source/Apple%20Pay/UnregisterDomain_abstract.dart';
+import 'package:kita_kerja/data/source/Apple%20Pay/UnregisterDomain_source.dart';
+import 'package:kita_kerja/providers/network_provider.dart';
 
 // final unregisterDomainSourceProvider = Provider<UnregisterDomainSource>(
 //   (ref) => UnregisterDomainSourceImpl(
@@ -9,3 +9,10 @@
 //     networkRetry: ref.read(networkRetryProvider),
 //   ),
 // );
+
+final unregisterDomainSourceProvider = Provider<UnregisterDomainSource>(
+  (ref) => UnregisterDomainSourceImpl(
+    networkRequest: ref.read(networkRequestProvider),
+    networkRetry: ref.read(networkRetryProvider),
+  ),
+);
